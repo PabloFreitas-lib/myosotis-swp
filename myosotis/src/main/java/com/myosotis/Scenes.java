fce19609af1ce649a32360e68c8de1968b9eddd6
Kasim Ali Shah <kasim1@uni-bremen.de>
2022-12-29 00:44:51 +0100

    kleine Updates

--- myosotis/src/main/java/com/myosotis/Scenes.java
+++ myosotis/src/main/java/com/myosotis/Scenes.java
@@ -8,14 +8,16 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.*;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Scenes {
     int initial_width = 1000;
     int initial_height = 700;
-    
-    Scene basicScene(Stage primaryStage){
+
+    Scene basicScene(Stage primaryStage, ArrayList<Node> nodesRight, ArrayList<Node> nodesLeft, ArrayList<Node> nodesTop, ArrayList<Node> nodesCenter, String title) {
         // Erstellt den Wurzelknoten und die Szene
-        VBox root1 = new VBox();
-        Scene scene = new Scene(root1, initial_width, initial_height);
+        VBox root = new VBox();
+        Scene scene = new Scene(root, initial_width, initial_height);
         return scene;
     }
     
